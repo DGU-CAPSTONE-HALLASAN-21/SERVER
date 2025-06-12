75f71892476a7929f36ffc46876ec15a0a05d2cb
eundeang <111877048+eundeang@users.noreply.github.com>
2025-06-13 04:05:20 +0900

    [feat] 답변 기능 구현

--- src/main/java/org/dgu/dto/chat/ReqChatCreate.java
+++ src/main/java/org/dgu/dto/chat/ReqChatCreate.java
@@ -3,8 +3,8 @@ package org.dgu.dto.chat;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record ReqChatCreate (
-        @Schema(description = "생성할 대화 이름", example = "새 대화")
-        String name
+        @Schema(description = "생성할 대화 내용", example = "새 대화")
+        String content
 ){
 
 }
